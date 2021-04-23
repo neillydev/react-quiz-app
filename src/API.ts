56f08a725ae17cec13bd79badc6c60f8c664c584
neillycubed <neillybusiness3@gmail.com>
2021-04-23 01:34:52 -0700

    add Question and QuestionState types for results

--- src/API.ts
+++ src/API.ts
@@ -1,11 +1,15 @@
+import { shuffleArray } from './utils/utils';
+
 export type Question = {
    category: string;
    correct_answer: string;
    difficulty: string;
-   incorrect_answer: string[];
+   incorrect_answers: string[];
    question: string;
    type: string; 
 }
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty{
     EASY = "easy",
@@ -17,5 +21,15 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
 
-    console.log(data);
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray(
+                [
+                    ...question.incorrect_answers, 
+                    question.correct_answer
+                ]
+            )
+        }
+    ))
 }
